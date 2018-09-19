e6ae4e7b7bff18504f13154ace663d4e4444cd1d
kathleen.e.burns@gmail.com <kathleen.e.burns@gmail.com>
2018-09-19 17:48:36 -0500

    removal of pseudocode

--- javascript/game.js
+++ javascript/game.js
@@ -13,7 +13,6 @@ console.log(ranLetter);
 function jsGuess() {
       ranLetter = letters[Math.floor(Math.random() * letters.length)];
       console.log(ranLetter);
-
 }
 
 
@@ -25,7 +24,6 @@ document.onkeyup = function (event) {
             won++;
             attempts = 10;
             usedArray = [];
-
       }
 
       
@@ -45,7 +43,6 @@ document.onkeyup = function (event) {
       if (usedArray.indexOf(playerGuess) >= 0) {
 
       } else {
-            
             usedArray.push(playerGuess);
             document.getElementById('playerGuess').innerHTML = usedArray;
             console.log(usedArray);
@@ -54,39 +51,5 @@ document.onkeyup = function (event) {
       document.getElementById('won').innerHTML = won;
       document.getElementById('lost').innerHTML = lost;
       document.getElementById('attempts').innerHTML = attempts;
-
 }
-
-// var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
-
-// var wins = 0;
-// var losses = 0;
-// var ties = 0;
-
-// var textDirections = document.getElementById("text-directions");
-// var userChoicaText = document.getElementById("userchoice-directions");
-// var computerChoiceText = document.getElementById ("computerchoice-text");
-// var winsText = document.getElementById("wins-text");
-// var lossesText = document.getElementById("losses-text");
-// var tiesText = document.getElementById("ties-text");
-
-// document.onkeyup = function(event) {
-//     var userGuess = event.key;
-//     var computerGuess = computerChoices[Math.floor(math.random() * computerChoices.length)];
-
-//     if (userGuess === computerGuess) {
-//         wins++
-//     }
-//     else if (userGuess != computerGuess) {
-//         losses++
-//     }
-
-// directionsText.textContent = "";
-
-// userTextChoice.textContent = "You chose: " + userGuess;
-// computerChoiceText.textContent = "Computer chose: " + computerGuess;
-// winsText.textContent = "wins: " + wins;
-// lossesText.textContent = "losses: " + losses;
-
-// }
 
